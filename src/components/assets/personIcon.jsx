8f6d85758f021f2da22cf465b00cf02c1a5817d7
mijakasia <katarzyna.nowak@rsqtechnologies.com>
2019-06-30 15:58:33 +0200

    active link state, icons and css

---
+++ src/components/assets/personIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+const PersonIcon = () => (
+  <FontAwesomeIcon icon={faUser} />
+);
+
+export default PersonIcon
