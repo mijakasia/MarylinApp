6166fe38ec1154a2a344e104e676460406a062b6
mijakasia <katarzyna.nowak@rsqtechnologies.com>
2019-06-30 15:12:10 +0200

    added onClick and background image to jumbotron

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -4,10 +4,9 @@ class Jumbotron extends Component {
 
   render() {
     return (
-      <div className="jumbotron menu-top__jumbotron">
-        <h1>Marylin Monroe</h1>
-        <p>“I don't mind living in a man's world, as long as I can be a woman in it.” </p>
-      </div>
+      <div
+        className="jumbotron menu-top__jumbotron"
+      />
     );
   }
 
